8c2dcf075232b7941f2558e77810c831993fc475
munteanudenis87 <munteanudenis87@gmail.com>
2026-03-05 16:08:18 +0100

    aggiunto actress card

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,38 @@ export default function App() {
     })
   }
   useEffect(actressesList, []);
-  
-  return (
-    <div>
 
-    </div>
+  return (
+    <section className="container">
+      <h1>Cast Fetching</h1>
+      <section className="actresses">
+        {actresses.map(actresses => (
+          <article key={actresses.id}>
+            
+            <div className="actress-info">
+              <p className="actress-name">
+                {actresses.name}
+              </p>
+              <p className="actress-birth-year">
+                {actresses.birth_year}
+              </p>
+              <p className="actress-nationality">
+                {actresses.nationality}
+              </p>
+              <p className="actress-biography">
+                {actresses.biography}
+              </p>
+              <div className="actress-image">
+              <img src={actresses.image} alt="actress image" />
+              </div>
+              <p className="actress-awards">
+                {actresses.awards}
+              </p>
+            </div>
+          </article>
+        ))}
+
+      </section>
+    </section>
   )
 }
