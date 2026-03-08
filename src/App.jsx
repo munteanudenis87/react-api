2af806e24e979830a0f3b602afadd7d8dbec1360
munteanudenis87 <munteanudenis87@gmail.com>
2026-03-08 11:35:45 +0100

    lista attori

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 
 export default function App() {
   const [actresses, setActresses] = useState([]);
+  const [actors, setActors] = useState([]);
 
   function actressesList () {
     axios.get("https://lanciweb.github.io/demo/api/actresses/")
@@ -12,39 +13,83 @@ export default function App() {
       console.log(response.data);
     })
   }
+  function actorsList () {
+    axios.get("https://lanciweb.github.io/demo/api/actors/")
+    .then((response) => {
+      setActors(response.data);
+      console.log(response.data);
+    })
+  }
   useEffect(actressesList, []);
+  useEffect(actorsList, []);
 
   return (
     <section className="container">
-      <h1>Cast Fetching</h1>
-      <section className="actresses">
-        {actresses.map(actresses => (
-          <article key={actresses.id}>
-            
-            <div className="actress-info">
-              <p className="actress-name">
-                {actresses.name}
-              </p>
-              <p className="actress-birth-year">
-                {actresses.birth_year}
-              </p>
-              <p className="actress-nationality">
-                {actresses.nationality}
-              </p>
-              <p className="actress-biography">
-                {actresses.biography}
-              </p>
-              <div className="actress-image">
-              <img src={actresses.image} alt="actress image" />
+      <section className='row'>
+        <section className="actress col-6">
+          <h1>Actresses List</h1>
+          {actresses.map(actresses => (
+            <article key={actresses.id}>
+              <div className="actress-info">
+                <p className="actress-name">
+                  {actresses.name}
+                </p>
+                <p className="actress-birth-year">
+                  {actresses.birth_year}
+                </p>
+                <p className="actress-nationality">
+                  {actresses.nationality}
+                </p>
+                <p className="actress-biography">
+                  {actresses.biography}
+                </p>
+                <p className="actress-awards">
+                  {actresses.awards}
+                </p>
               </div>
-              <p className="actress-awards">
-                {actresses.awards}
-              </p>
-            </div>
-          </article>
-        ))}
-
+              <div className="actress-image">
+                <img src={actresses.image} className="actress-img" alt={actresses.name} 
+                  onError={(event) => {
+                    event.currentTarget.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxCTwsgxYd4LhL6lORmydMfwB6unVuBUv0Q97l6hOSE7iJYJEpg2nDXOGxQISWeTzOnSE&usqp=CAU"
+                  }}
+                />
+              </div>
+            </article>
+          ))}
+        </section>
+        <section className="actors col-6">
+          <h1>Actors List</h1>
+          {actors.map(actors => (
+            <article key={actors.id}>
+              <div className="actress-info">
+                <p className="actress-name">
+                  {actors.name}
+                </p>
+                <p className="actress-birth-year">
+                  {actors.birth_year}
+                </p>
+                <p className="actress-nationality">
+                  {actors.nationality}
+                </p>
+                <p className="actress-biography">
+                  {actors.biography}
+                </p>
+                <p className="actress-awards">
+                  {actors.awards}
+                </p>
+              </div>
+              <div className="actress-image">
+                <img src={actors.image} className="actress-img" alt={actors.name} 
+                  onError={(event) => {
+                    event.currentTarget.src = "https://cdn.vectorstock.com/i/500p/28/59/flat-style-male-avatar-person-icon-vector-59492859.jpg"
+                  }}
+                />
+              </div>
+            </article>
+          ))}
+        </section>
       </section>
+      <hr />
     </section>
   )
 }
